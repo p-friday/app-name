929fa230df92f76a10d63e369e4a0fe09fb92c44
kiki8388 <70375140+kiki8388@users.noreply.github.com>
2024-01-15 11:58:37 +0100

    update AuthProvider.tsx

--- src/components/Auth/AuthProvider.tsx
+++ src/components/Auth/AuthProvider.tsx
@@ -3,7 +3,7 @@ import { useCookies } from "react-cookie";
 function AuthProvider() {
     const [cookies] = useCookies(['user'])
     if (cookies.user)
-        return {"Authorization": `Bearer ${cookies.user}`}
+        return {"Authorization": `Bearer ${cookies.user.token}`}
     else
         return {};
 }
