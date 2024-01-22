d4415d9a55ee58e91eb6a417fdfe4d58d6a11c42
Peter Piatek <piotr.piiatek@gmail.com>
2024-01-22 11:37:13 +0100

    fix

--- src/components/Plans/plans.tsx
+++ src/components/Plans/plans.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
 import {
   Button,
   Table,
